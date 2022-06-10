b8700114243b86a6bb24f7fb2db74715ed9c5e19
Apoorv Mintri <apoorv.mintri@electronikmedia.com>
2022-06-10 19:21:29 +0530

    environment threads

---
+++ AzureBlob.HighSpeed.FileUpload/DetailsFunction.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlob.HighSpeed.FileUpload
+{
+    public static class DetailsFunction
+    {
+        public static IActionResult GetThreads(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "get/threads")] HttpRequest req,
+            ILogger log)
+        {
+            var response = $"Processor Count: {Environment.ProcessorCount}\nThreads: {Environment.ProcessorCount * 8}";
+            return new OkObjectResult(response);
+        }
+    }
+}
